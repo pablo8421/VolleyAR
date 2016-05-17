66a09a0fbb618026dba2a54533a5a132f137bd20
pablo8421 <pablo_jsd-8@hotmail.com>
2016-05-16 23:05:09 -0600

    Bolita que se mueve de lado a lado :)

---
+++ Assets/Sphere_movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere_movement : MonoBehaviour {
+
+    Vector3 tVector;
+    float boxLimit = 1.0f;
+    float delta = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+        tVector = new Vector3(Random.Range(-delta, delta),
+                              Random.Range(-delta, delta),
+                              Random.Range(-delta, delta));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 t = gameObject.transform.position;
+        if (Mathf.Abs(t.x) > boxLimit 
+         || Mathf.Abs(t.y) > boxLimit 
+         || Mathf.Abs(t.z) > boxLimit)
+        {
+            tVector = new Vector3(Random.Range(-delta, delta),
+                                  Random.Range(-delta, delta),
+                                  Random.Range(-delta, delta));
+        }
+        gameObject.transform.Translate(tVector);
+	}
+}
